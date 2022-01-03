b314e63ef48786535287648a50bc0fb5a028c061
Kamil Kaczmarczyk <kizeenn@gmail.com>
2022-01-03 15:46:03 +0100

    Replace placeholders with real data

--- src/components/todos.tsx
+++ src/components/todos.tsx
@@ -1,30 +1,40 @@
 import { StarIcon } from "@heroicons/react/solid";
-import { EmojiHappyIcon } from "@heroicons/react/outline";
+import { EmojiHappyIcon, EmojiSadIcon } from "@heroicons/react/outline";
+import { Todo } from "../pages";
 
-export default function Todos() {
+export default function Todos({ todos }: { todos: Todo[] }) {
   return (
-    <div className="w-full px-20 flex items-center mt-10">
-      <div className="w-full shadow-2xl bg-white p-3 flex">
-        <div className="flex items-center border-r border-black w-1/2">
-          <StarIcon className="h-10 w-10 text-yellow-400" />
-          <div className="ml-5">
-            <h1 className="font-bold text-lg truncate max-w-md">title</h1>
-            <h2 className="text-sm">user id</h2>
-          </div>
-        </div>
-
-        <div className="w-1/2 flex justify-between">
-          <div className="flex ml-5 items-center">
-            <p className="font-bold text-lg">completed:</p>
-            <EmojiHappyIcon className="h-8 w-8 text-green-500 ml-1" />
+    <div className="w-full px-20 items-center mt-10">
+      {todos.map((todo) => (
+        <div key={todo.id} className="w-full shadow-xl bg-white p-3 flex mt-5">
+          <div className="flex justify-between items-center border-r border-black w-1/2 px-5">
+            <StarIcon className="h-10 w-10 text-yellow-400" />
+            <h1 className="font-bold text-lg truncate max-w-md ml-5">
+              {todo.title}
+            </h1>
+            <div className="flex items-end">
+              <p className="text-lg mr-2">user id</p>
+              <p className="font-bold text-3xl">{todo.userId}</p>
+            </div>
           </div>
 
-          <div className="flex items-end ml-10">
-            <p className="text-lg">task id</p>
-            <p className="text-6xl font-bold">1</p>
+          <div className="w-1/2 flex justify-between px-5">
+            <div className="flex items-center">
+              <p className="font-bold text-lg">completed:</p>
+              {todo.completed ? (
+                <EmojiHappyIcon className="h-8 w-8 text-green-600 ml-1" />
+              ) : (
+                <EmojiSadIcon className="h-8 w-8 text-rose-500 ml-1" />
+              )}
+            </div>
+
+            <div className="flex items-end">
+              <p className="text-lg mr-2">task id</p>
+              <p className="text-3xl font-bold">{todo.id}</p>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,32 @@ import type { NextPage } from "next";
 import Dashboard from "../components/dashboard";
 import Filters from "../components/filters";
 import Todos from "../components/todos";
+import { GetServerSideProps } from "next";
 
-const Home: NextPage = () => {
+export type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const todos: Todo[] = await response.json();
+
+  return {
+    props: {
+      todos: todos,
+    },
+  };
+};
+
+const Home: NextPage<{ todos: Todo[] }> = ({ todos }) => {
   return (
     <>
       <Dashboard />
       <Filters />
-      <Todos />
+      <Todos todos={todos} />
     </>
   );
 };
